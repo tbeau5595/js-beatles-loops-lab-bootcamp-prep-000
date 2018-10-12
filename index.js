7eb879c0cb71504c4709f34b227c160997f196ba
Trey <tbeau5595@hotmail.com>
2018-10-12 05:01:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var array = [];
   var i = 0;
- while (array.length < facts.length) {
+  while (array.length < facts.length) {
     array.push(facts[i] + "!!!");
     i++;
   }
