60cfb8b150842f369692c0dc01f3cfb0aa565305
Trey <tbeau5595@hotmail.com>
2018-10-12 04:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 Function beatlesplay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicans.length; i++) {
-    array.push(musicians[i] )
+    array.push(musicians[i] + plays + instruments[i])
   }
 }
