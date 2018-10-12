f843e0353af9a7feb74273bea7db11d76a493489
Trey <tbeau5595@hotmail.com>
2018-10-12 04:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"]
-  var array
+  "He hated the sound of his own voice"];
+  var array = [];
+  
 }
